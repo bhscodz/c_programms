8b1d05cffe4894b9bf4439b6bdb5be2c11d3d490
bhaskar <bhscodz7@gmail.com>
2024-08-30 23:53:04 +0530

    added comments

--- swap_with_third.c
+++ swap_with_third.c
@@ -1,1 +1,17 @@
+// Fri, Aug 30, 2024 11:51:39 PM
 // some changes
+// some more changes
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("enter the first variable,a");
+    scanf("%d",&a);
+    printf("enter the second variable,b");
+    scanf("%d",&b);
+    c=a;
+    a=b;
+    b=c;
+    printf("value of first variable after swaping,a %d \n",a);
+    printf("value of second variable after swaping, %d \n",b);
+}     
+
